7e1c41b33a0f9bf8c5848bb8d38c7dd7616fafce
Christopher Staymates <77902343+HappySteward@users.noreply.github.com>
2021-03-17 14:05:49 -0700

    Deleted previous entry and started a Pokemon Array from Scracth.
    Added an array containing name/type/height.
    Pokemon name and variables added:
    Charmander/fire/0.6
    
    bulbasaur/grass/poison/0.7
    
    squirtle/water/0.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,1 @@
-alert('Hello world');
-let favoriteFood = 'sushi';
-document.write(favoriteFood);
+pokemonList=[{name: "charmander", type: "fire", height: "0.6"},{name: "bulbasaur", type:["grass","poison"], height:"0.7"},{name:"squirtle", type:"water",height:"0.5"}]
