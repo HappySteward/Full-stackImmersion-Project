2c0695fa4598267ba2c9316fcc6c9b7c7afce512
Christopher Staymates <77902343+HappySteward@users.noreply.github.com>
2021-03-25 19:12:30 -0700

    Added a loop to display pokemon names and height.
    Displays names of pokemon with heights and it writes wow, thats big for any pokemon = or above 0.7

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-pokemonList=[{name: "charmander", type: "fire", height: "0.6"},{name: "bulbasaur", type:["grass","poison"], height:"0.7"},{name:"squirtle", type:"water",height:"0.5"}]
+pokemonList=[
+  {name: "charmander", type: "fire", height: "0.6"},
+  {name: "bulbasaur", type:["grass","poison"], height:"0.7"},
+  {name:"squirtle", type:"water",height:"0.5"}
+]
+
+let pokemonSizeThreshold = 0.7;
+
+for(let i = 0; i <= pokemonList.length; i++){
+  let pokemonString = `${pokemonList[i].name} ( height: ${ pokemonList[i].height} )`;
+  let pokemon = pokemonList[i].height >= pokemonSizeThreshold ? `<P> ${pokemonString} - Wow, that's big! </p>` : `<p> ${pokemonString} </p>`;
+  document.write(pokemon);
+}
